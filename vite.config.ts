ce9769a9428b8420a1791a07121102a7160cb54d
User <user@example.com>
2026-05-01 21:07:04 +0300

    Fix Github Pages base path

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   plugins: [react()],
-  base: mode === 'development' ? '/' : '/bookvisa/',
+   base: '/bookvisa/',
   server: {
     port: 3000,
     open: true,
