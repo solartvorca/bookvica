38e031baeed898056cbcb8b7e8ead8a2f26ab4cb
User <user@example.com>
2026-05-02 20:53:19 +0300

    Fix: add basename for router and assets

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   plugins: [react()],
-   base: '/bookvica/',
+  base: '/bookvica/',
   server: {
     port: 3000,
     open: true,
   },
   build: {
     outDir: 'docs',
+    assetsDir: 'assets',
+    emptyOutDir: true,
     sourcemap: false,
   },
 }))
